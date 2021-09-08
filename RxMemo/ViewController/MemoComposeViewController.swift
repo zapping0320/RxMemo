bb82a48db0ec109975c9ecb33625674e6f68fa85
John DongHyun Kim <zapping0320@gmail.com>
2021-09-08 22:22:41 +0900

    MemoComposeViewController 구현

--- RxMemo/ViewController/MemoComposeViewController.swift
+++ RxMemo/ViewController/MemoComposeViewController.swift
@@ -6,17 +6,54 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
+import NSObject_Rx
 
 class MemoComposeViewController: UIViewController, ViewModelBindableType {
     
     var viewModel: MemoComposeViewModel!
 
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    
+    @IBOutlet weak var contentTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
     }
     
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        viewModel.initiateText
+            .drive(contentTextView.rx.text)
+            .disposed(by: rx.disposeBag)
+        
+        cancelButton.rx.action = viewModel.cancelAction
+        saveButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withLatestFrom(contentTextView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: rx.disposeBag)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        contentTextView.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if contentTextView.isFirstResponder {
+            contentTextView.resignFirstResponder()
+        }
     }
 }
