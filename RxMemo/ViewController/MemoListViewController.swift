919a687c1bbe070701848c9c53c4f7621d81086f
John DongHyun Kim <zapping0320@gmail.com>
2021-09-10 13:16:33 +0900

    detailAction 구현
    - scenecoordinator 버그가 있는 상태에서 화면 전환이 이루어지지 않는다.

--- RxMemo/ViewController/MemoListViewController.swift
+++ RxMemo/ViewController/MemoListViewController.swift
@@ -34,5 +34,12 @@ class MemoListViewController: UIViewController, ViewModelBindableType {
         
         addButton.rx.action = viewModel.makeCreateAction()
         
+        Observable.zip(listTableView.rx.modelSelected(Memo.self), listTableView.rx.itemSelected)
+            .do(onNext: { [unowned self] (_, indexPath) in
+                self.listTableView.deselectRow(at: indexPath, animated: true)
+            })
+            .map { $0.0 }
+            .bind(to: viewModel.detailAction.inputs)
+            .disposed(by: rx.disposeBag)
     }
 }
