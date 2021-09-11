5be45aac958ac4ba591829b4b08ef4be1b743ee6
John DongHyun Kim <zapping0320@gmail.com>
2021-09-11 23:12:34 +0900

    공유 기능을 tap 속성으로 구현함

--- RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/ViewController/MemoDetailViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class MemoDetailViewController: UIViewController, ViewModelBindableType {
 
@@ -59,6 +61,17 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
         
         
         editButton.rx.action = viewModel.makeEditAction()
+        
+        shareButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .subscribe(onNext : { [weak self] _ in
+                guard let memo = self?.viewModel.memo.content else { return }
+                
+                let vc = UIActivityViewController(activityItems: [memo], applicationActivities: nil)
+                self?.present(vc, animated: true, completion: nil)
+                    
+            })
+            .disposed(by: rx.disposeBag)
         
     }
 
